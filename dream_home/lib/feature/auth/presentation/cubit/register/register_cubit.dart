f34463707ffcb39a625a0c989b070fc932018659
kareem_m0hmed <km7404714@gmail.com>
2024-12-26 20:25:50 +0200

    finish_register

---
+++ dream_home/lib/feature/auth/presentation/cubit/register/register_cubit.dart
@@ -0,0 +1,15 @@
+import 'package:equatable/equatable.dart';
+import 'package:flutter/material.dart';
+import 'package:flutter_bloc/flutter_bloc.dart';
+
+part 'register_state.dart';
+
+class RegisterCubit extends Cubit<RegisterState> {
+  RegisterCubit() : super(RegisterInitial());
+
+  final TextEditingController userNameController = TextEditingController();
+  final TextEditingController emailController = TextEditingController();
+  final TextEditingController passwordController = TextEditingController();
+  final TextEditingController confirmPasswordController =
+      TextEditingController();
+}
