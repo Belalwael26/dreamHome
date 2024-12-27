32199f9c8e09a2910b97d46beef5c578877191af
kareem_m0hmed <km7404714@gmail.com>
2024-12-28 00:51:25 +0200

    finish_change_password_and_change_number

--- dream_home/lib/feature/customer_profile/presentation/widget/change_number_screen.dart
+++ dream_home/lib/feature/customer_profile/presentation/widget/change_number_screen.dart
@@ -1,10 +1,45 @@
+import 'package:dream_home/core/extension/extension.dart';
+import 'package:dream_home/feature/auth/presentation/widget/custom_text_form_filed.dart';
 import 'package:flutter/material.dart';
+import 'package:go_router/go_router.dart';
+
+import '../../../../core/constant/app_sized.dart';
+import '../../../../core/styles/app_text_style.dart';
+import '../../../../core/utils/app_color.dart';
+import '../../../../core/widget/custom_app_button.dart';
 
 class ChangeNumberScreen extends StatelessWidget {
   const ChangeNumberScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Padding(
+        padding: const EdgeInsets.symmetric(horizontal: 16, vertical: 40),
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.start,
+          children: [
+            Icon(Icons.arrow_back_ios, color: AppColor.beanut)
+                .onTap(context.pop),
+            height(24),
+            Text(
+              "Change Phone Number",
+              style: AppTextStyle.style24.copyWith(color: AppColor.lightblack),
+            ),
+            height(50),
+            CustomTextFormFiled(
+              controller: TextEditingController(),
+              hintText: "Phone Number",
+            ),
+            Spacer(),
+            CustomAppButton(
+              text: "Send",
+              containerColor: AppColor.beanut,
+              textColor: AppColor.white,
+            )
+          ],
+        ),
+      ),
+    );
   }
 }
