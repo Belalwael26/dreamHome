1d9fa3a7d13539c554da97aea8312e7b0108e0e0
kareem_m0hmed <km7404714@gmail.com>
2025-01-02 13:31:40 +0200

    finish_register_screen

---
+++ dream_home/lib/feature/auth/data/repo/register/register_repo_impl.dart
@@ -0,0 +1,49 @@
+import 'dart:developer';
+
+import 'package:cloud_firestore/cloud_firestore.dart';
+import 'package:dartz/dartz.dart';
+import 'package:dream_home/core/errors/errors.dart';
+import 'package:dream_home/feature/auth/data/model/user_model.dart';
+import 'package:dream_home/feature/auth/data/repo/register/register_repo.dart';
+import 'package:firebase_auth/firebase_auth.dart';
+
+class RegisterRepoImpl implements RegisterRepo {
+  @override
+  Future<Either<Failure, UserModel>> register({
+    required String name,
+    required String email,
+    required String password,
+    required String confirmPassword,
+  }) async {
+    try {
+      UserCredential userCredential =
+          await FirebaseAuth.instance.createUserWithEmailAndPassword(
+        email: email,
+        password: password,
+      );
+
+      User? user = userCredential.user;
+      final firestore = FirebaseFirestore.instance;
+
+      if (user != null) {
+        Map<String, dynamic> userData = {
+          'uid': user.uid,
+          'email': email,
+          'name': name,
+          'password': password,
+          'createdAt': FieldValue.serverTimestamp(),
+        };
+        await firestore.collection('users').doc(user.uid).set(userData);
+        DocumentSnapshot<Map<String, dynamic>> userDoc = await firestore
+            .collection('users')
+            .doc(userCredential.user?.uid)
+            .get();
+        log("Doce: $userDoc");
+        return Right(UserModel());
+      }
+      return Left(ServerFailure("User creation failed."));
+    } catch (e) {
+      return Left(ServerFailure(e.toString()));
+    }
+  }
+}
--- dream_home/lib/feature/auth/presentation/views/register_screen.dart
+++ dream_home/lib/feature/auth/presentation/views/register_screen.dart
@@ -1,5 +1,9 @@
+import 'package:dream_home/core/function/show_toast.dart';
+import 'package:dream_home/core/function/validation.dart';
 import 'package:dream_home/core/styles/app_text_style.dart';
 import 'package:dream_home/core/utils/app_color.dart';
+import 'package:dream_home/core/widget/custom_loader.dart';
+import 'package:dream_home/di.dart';
 import 'package:flutter/material.dart';
 import 'package:flutter_bloc/flutter_bloc.dart';
 import 'package:go_router/go_router.dart';
@@ -17,81 +21,138 @@ class RegisterScreen extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: appBar(
-        context,
-        title: "Sign UP",
-        automaticallyImplyLeading: false,
-      ),
-      body: BlocProvider(
-        create: (context) => RegisterCubit(),
-        child: BlocBuilder<RegisterCubit, RegisterState>(
+    return BlocProvider(
+        create: (context) => RegisterCubit(getIt()),
+        child: BlocConsumer<RegisterCubit, RegisterState>(
+          listener: (context, state) {
+            if (state is RegisterSuccess) {
+              showToast(
+                message: "Register Success",
+                backgroundColor: AppColor.beanut,
+              );
+              context.pushReplacement(Routes.whoareyou);
+            } else if (state is RegisterError) {
+              showToast(
+                message: state.message,
+                backgroundColor: AppColor.redED,
+              );
+            }
+          },
           builder: (context, state) {
             final cubit = context.read<RegisterCubit>();
-            return SingleChildScrollView(
-              child: Padding(
-                padding: const EdgeInsets.symmetric(horizontal: 16),
-                child: Column(
-                  crossAxisAlignment: CrossAxisAlignment.start,
-                  children: [
-                    height(heightSize(context) * 0.1),
-                    Text(
-                      "FullName ",
-                      style: AppTextStyle.style14,
-                    ),
-                    height(4),
-                    CustomTextFormFiled(
-                      hintText: "name",
-                      controller: cubit.emailController,
-                    ),
-                    height(16),
-                    Text(
-                      "Email",
-                      style: AppTextStyle.style14,
-                    ),
-                    height(4),
-                    CustomTextFormFiled(
-                      hintText: "demo@mail.com",
-                      controller: cubit.emailController,
-                    ),
-                    height(16),
-                    Text(
-                      "Password",
-                      style: AppTextStyle.style14,
-                    ),
-                    height(4),
-                    CustomTextFormFiled(
-                      hintText: "**********************",
-                      controller: cubit.passwordController,
-                    ),
-                    height(16),
-                    Text(
-                      "Confirm Password",
-                      style: AppTextStyle.style14,
-                    ),
-                    height(4),
-                    CustomTextFormFiled(
-                      hintText: "**********************",
-                      controller: cubit.passwordController,
-                    ),
-                    height(16),
-                    CustomAppButton(
-                      text: "Sign UP ",
-                      containerColor: AppColor.beanut,
-                      textColor: AppColor.white,
-                      onPressed: () {
-                        context.pushReplacement(Routes.whoareyou);
-                      },
-                    ),
-                    height(heightSize(context) * 0.05),
-                    const CustomAuthText(isLoadgin: false),
-                  ],
+            return Scaffold(
+                appBar: appBar(
+                  context,
+                  title: "Sign UP",
+                  automaticallyImplyLeading: false,
                 ),
-              ),
-            );
+                body: state is RegisterLoading
+                    ? CustomLoader()
+                    : SingleChildScrollView(
+                        child: Padding(
+                          padding: const EdgeInsets.symmetric(horizontal: 16),
+                          child: Form(
+                            key: cubit.formKey,
+                            child: Column(
+                              crossAxisAlignment: CrossAxisAlignment.start,
+                              children: [
+                                height(heightSize(context) * 0.1),
+                                Text(
+                                  "FullName ",
+                                  style: AppTextStyle.style14,
+                                ),
+                                height(4),
+                                CustomTextFormFiled(
+                                  hintText: "name",
+                                  controller: cubit.userNameController,
+                                  validator: (val) =>
+                                      AppValidation.displayNameValidator(
+                                          cubit.userNameController.text),
+                                ),
+                                height(16),
+                                Text(
+                                  "Email",
+                                  style: AppTextStyle.style14,
+                                ),
+                                height(4),
+                                CustomTextFormFiled(
+                                  hintText: "demo@mail.com",
+                                  controller: cubit.emailController,
+                                  validator: (val) =>
+                                      AppValidation.emailValidator(
+                                          cubit.emailController.text),
+                                ),
+                                height(16),
+                                Text(
+                                  "Password",
+                                  style: AppTextStyle.style14,
+                                ),
+                                height(4),
+                                CustomTextFormFiled(
+                                  hintText: "**********************",
+                                  controller: cubit.passwordController,
+                                  obscureText: cubit.passwordObsecureText,
+                                  validator: (val) =>
+                                      AppValidation.passwordVaildtor(
+                                          cubit.passwordController.text),
+                                  suffixIcon: IconButton(
+                                    icon: Icon(
+                                      cubit.passwordObsecureText
+                                          ? Icons.visibility_outlined
+                                          : Icons.visibility_off_outlined,
+                                      color: AppColor.beanut,
+                                    ),
+                                    onPressed: () {
+                                      cubit.changepasswordObsecureText();
+                                    },
+                                  ),
+                                ),
+                                height(16),
+                                Text(
+                                  "Confirm Password",
+                                  style: AppTextStyle.style14,
+                                ),
+                                height(4),
+                                CustomTextFormFiled(
+                                  validator: (val) =>
+                                      AppValidation.confirmPasswordVaildtor(
+                                    password:
+                                        cubit.confirmPasswordController.text,
+                                    value: cubit.passwordController.text,
+                                  ),
+                                  hintText: "**********************",
+                                  controller: cubit.confirmPasswordController,
+                                  obscureText:
+                                      cubit.confirmPasswordObsecureText,
+                                  suffixIcon: IconButton(
+                                    icon: Icon(
+                                      cubit.confirmPasswordObsecureText
+                                          ? Icons.visibility_outlined
+                                          : Icons.visibility_off_outlined,
+                                      color: AppColor.beanut,
+                                    ),
+                                    onPressed: () {
+                                      cubit.changeconfirmPasswordObsecureText();
+                                    },
+                                  ),
+                                ),
+                                height(16),
+                                CustomAppButton(
+                                  text: "Sign UP ",
+                                  containerColor: AppColor.beanut,
+                                  textColor: AppColor.white,
+                                  onPressed: () {
+                                    cubit.register();
+                                  },
+                                ),
+                                height(heightSize(context) * 0.05),
+                                const CustomAuthText(isLoadgin: false),
+                              ],
+                            ),
+                          ),
+                        ),
+                      ));
           },
-        ),
-      ),
-    );
+        ));
   }
 }
