9a71fdc50869a2c4e2319fca47e051fad03f9eac
kareem_m0hmed <km7404714@gmail.com>
2025-01-07 18:11:28 +0200

    add_info

--- dream_home/lib/feature/customer_profile/presentation/widget/custom_profile_info.dart
+++ dream_home/lib/feature/customer_profile/presentation/widget/custom_profile_info.dart
@@ -1,14 +1,40 @@
+import 'dart:developer';
+
 import 'package:dream_home/core/constant/app_sized.dart';
 import 'package:dream_home/core/extension/extension.dart';
 import 'package:dream_home/core/utils/app_color.dart';
+import 'package:dream_home/feature/auth/data/model/user_model.dart';
 import 'package:dream_home/feature/auth/presentation/widget/custom_text_form_filed.dart';
 import 'package:flutter/material.dart';
 import 'package:go_router/go_router.dart';
 
+import '../../../../core/cache/user_info_cache.dart';
 import 'custom_add_profile_stack.dart';
 
-class CustomProfileInfo extends StatelessWidget {
+class CustomProfileInfo extends StatefulWidget {
   const CustomProfileInfo({super.key});
+
+  @override
+  State<CustomProfileInfo> createState() => _CustomProfileInfoState();
+}
+
+class _CustomProfileInfoState extends State<CustomProfileInfo> {
+  UserModel? _user;
+
+  @override
+  void initState() {
+    load();
+    super.initState();
+  }
+
+  Future<void> load() async {
+    UserModel? user = await getUserFromSharedPreferences();
+    setState(() {
+      _user = user;
+    });
+    log("$user");
+    log("${user!.name}");
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -29,13 +55,13 @@ class CustomProfileInfo extends StatelessWidget {
             ),
             height(50),
             CustomTextFormFiled(
-                controller: TextEditingController(text: "Name")),
+                controller: TextEditingController(text: _user?.name)),
             height(16),
             CustomTextFormFiled(
-                controller: TextEditingController(text: "Phone ")),
+                controller: TextEditingController(text: _user?.phone)),
             height(16),
             CustomTextFormFiled(
-                controller: TextEditingController(text: "Email")),
+                controller: TextEditingController(text: _user?.email)),
           ],
         ),
       ),
